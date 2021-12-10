f3e675993efa882a03fcee6a37f31cc74bfe622a
Jason Hargrove <rijaha@gmail.com>
2021-12-09 19:27:39 -0600

    forEach example.

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const log = console.log;
 // Foreach takes a callback function an runs that callback function on each element of the array one by one.
 let sample = ['Richard', 'Jason', 'Hargrove'];
 
-sample.forEach((elem, index) => log(`${elem} comes at ${index}`));
+// sample.forEach((elem, index) => log(`${elem} comes at ${index}`));
 
 // forEach works as a traditional for loop. Looping over the array and providing you array elements to do operation on them.
 
+function sampleChange(arr) {
+    arr.forEach((elem, index) => {
+        return (index % 2 !== 0 ? log(elem = "Hi. I'm not divisible by 2") : log(elem.toLowerCase()))
+    });
+}
+sampleChange(sample);
